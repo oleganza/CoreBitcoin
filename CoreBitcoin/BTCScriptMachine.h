122f51b739ecddd6b69232d171a3a9b4d988bbbd
Oleg Andreev <oleganza@gmail.com>
2013-08-14 00:20:27 +0200

    wip on script

---
+++ CoreBitcoin/BTCScriptMachine.h
@@ -0,0 +1,13 @@
+//
+//  BTCScriptMachine.h
+//  CoreBitcoin
+//
+//  Created by Oleg Andreev on 8/13/13.
+//  Copyright (c) 2013 Oleg Andreev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTCScriptMachine : NSObject
+
+@end
---
+++ CoreBitcoin/BTCScriptMachine.m
@@ -0,0 +1,13 @@
+//
+//  BTCScriptMachine.m
+//  CoreBitcoin
+//
+//  Created by Oleg Andreev on 8/13/13.
+//  Copyright (c) 2013 Oleg Andreev. All rights reserved.
+//
+
+#import "BTCScriptMachine.h"
+
+@implementation BTCScriptMachine
+
+@end
