afc10e99b2910d89011ab581df5f434d47a99052
Oleg Andreev <oleganza@gmail.com>
2013-08-30 22:11:15 +0200

    documentation fix

--- CoreBitcoin/BTCData.h
+++ CoreBitcoin/BTCData.h
@@ -40,7 +40,7 @@ void BTCDataClear(NSMutableData* data);
 // If the argument is nil, returns nil.
 NSData* BTCSHA1(NSData* data);
 NSData* BTCSHA256(NSData* data);
-NSData* BTCHash256(NSData* data); // == SHA256(SHA256(data)) (aka Hash or Hash256 in BitcoinQT)
+NSData* BTCHash256(NSData* data); // == SHA256(SHA256(data)) (aka Hash in BitcoinQT)
 
 #if BTCDataRequiresOpenSSL
 // RIPEMD160 today is provided only by OpenSSL. SHA1 and SHA2 are provided by CommonCrypto framework.
