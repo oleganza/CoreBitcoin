51c1c4555fca861d54cbfa82e5abb2f98bff580a
bjano <bjano@dontspam.me>
2013-12-04 15:03:03 +0100

    frameworks with the same name

--- build_libraries.sh
+++ build_libraries.sh
@@ -53,3 +53,38 @@ xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinOSX -configuration 
 #clean up
 rm -rf build/CoreBitcoin.build
 
+
+
+# At this point all the libraries and frameworks are built and placed in the ./build 
+# directory with names ending with -IOS and -OSX indicating their architectures. The 
+# rest of the script renames them to have the same name without these suffixes. 
+
+# If you build your project in a way that you would rather have the names differ, you 
+# can uncomment the next line and stop the build process here.
+
+#exit
+
+
+#moving the result to a separate location
+
+BINARIES_TARGETDIR="binaries"
+
+rm -rf ${BINARIES_TARGETDIR}
+
+mkdir ${BINARIES_TARGETDIR}
+mkdir ${BINARIES_TARGETDIR}/OSX
+mkdir ${BINARIES_TARGETDIR}/iOS
+
+#move and rename the frameworks
+mv build/CoreBitcoinOSX.framework ${BINARIES_TARGETDIR}/OSX/CoreBitcoin.framework
+mv ${BINARIES_TARGETDIR}/OSX/CoreBitcoin.framework/CoreBitcoinOSX ${BINARIES_TARGETDIR}/OSX/CoreBitcoin.framework/CoreBitcoin
+
+mv build/CoreBitcoinIOS.framework ${BINARIES_TARGETDIR}/iOS/CoreBitcoin.framework
+mv ${BINARIES_TARGETDIR}/iOS/CoreBitcoin.framework/CoreBitcoinIOS ${BINARIES_TARGETDIR}/iOS/CoreBitcoin.framework/CoreBitcoin
+
+#move and rename the static libraries
+mv build/libCoreBitcoinIOS.a ${BINARIES_TARGETDIR}/iOS/libCoreBitcoin.a
+mv build/libCoreBitcoinOSX.a ${BINARIES_TARGETDIR}/OSX/libCoreBitcoin.a
+
+#move the headers
+mv build/include ${BINARIES_TARGETDIR}/include
