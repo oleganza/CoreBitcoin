9ab5bb3599c32e8495383a2849fe768826b2ae3e
Oleg Andreev <oleganza@gmail.com>
2014-02-23 12:43:13 +0100

    comments for build_libraries.sh

--- build_libraries.sh
+++ build_libraries.sh
@@ -1,35 +1,40 @@
 #!/bin/sh
 
-#start with a clean sheet
+# Cleanup to start with a blank slate
 
+mkdir -p build
 rm -rf build/CoreBitcoin.build
 rm -rf build/*.framework
 rm -rf build/*.a
 
+# Update all headers to produce up-to-date combined headers.
 
-#build the iOS static libraries once for the simulator and once for the actual devices
+./update_headers.rb
+
+# Build iOS static libraries for simulator and for devices
 
 xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinIOSlib -configuration Release -sdk iphonesimulator
 mv build/libCoreBitcoinIOS.a build/libCoreBitcoinIOS-simulator.a
+
 xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinIOSlib -configuration Release -sdk iphoneos
 mv build/libCoreBitcoinIOS.a build/libCoreBitcoinIOS-device.a
 
-#merge them and clean up
+# Merge simulator and device libs into one
 
 lipo build/libCoreBitcoinIOS-device.a build/libCoreBitcoinIOS-simulator.a -create -output build/libCoreBitcoinIOS.a
 rm build/libCoreBitcoinIOS-simulator.a
 rm build/libCoreBitcoinIOS-device.a
 
-
-#build the frameworks once for the simulator, once for the devices
+# Build the iOS frameworks for simulator and for devices
 
 rm -f build/CoreBitcoinIOS*.framework
 
 xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinIOS -configuration Release -sdk iphonesimulator
 mv build/CoreBitcoinIOS.framework build/CoreBitcoinIOS-simulator.framework
+
 xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinIOS -configuration Release -sdk iphoneos
 
-#merge the libraries from the bundles
+# Merge the libraries inside the frameworks
 
 mv build/CoreBitcoinIOS-simulator.framework/CoreBitcoinIOS build/CoreBitcoinIOS.framework/CoreBitcoinIOS-simulator
 mv build/CoreBitcoinIOS.framework/CoreBitcoinIOS build/CoreBitcoinIOS.framework/CoreBitcoinIOS-device
@@ -37,22 +42,20 @@ mv build/CoreBitcoinIOS.framework/CoreBitcoinIOS build/CoreBitcoinIOS.framework/
 lipo build/CoreBitcoinIOS.framework/CoreBitcoinIOS-simulator build/CoreBitcoinIOS.framework/CoreBitcoinIOS-device \
 		-create -output build/CoreBitcoinIOS.framework/CoreBitcoinIOS
 		
-#delete the unnecessary remains
+# Delete the intermediate files
 		
 rm build/CoreBitcoinIOS.framework/CoreBitcoinIOS-device
 rm build/CoreBitcoinIOS.framework/CoreBitcoinIOS-simulator
 rm -rf build/CoreBitcoinIOS-simulator.framework
 
 
-
-#build for OSX
+# Build for OS X
 
 xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinOSXlib -configuration Release
-xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinOSX -configuration Release
+xcodebuild -project CoreBitcoin.xcodeproj -target CoreBitcoinOSX    -configuration Release
 
-#clean up
+# Clean up
 rm -rf build/CoreBitcoin.build
-
 
 
 # At this point all the libraries and frameworks are built and placed in the ./build 
@@ -65,7 +68,7 @@ rm -rf build/CoreBitcoin.build
 #exit
 
 
-#moving the result to a separate location
+# Moving the result to a separate location
 
 BINARIES_TARGETDIR="binaries"
 
@@ -75,16 +78,24 @@ mkdir ${BINARIES_TARGETDIR}
 mkdir ${BINARIES_TARGETDIR}/OSX
 mkdir ${BINARIES_TARGETDIR}/iOS
 
-#move and rename the frameworks
+# Move and rename the frameworks
 mv build/CoreBitcoinOSX.framework ${BINARIES_TARGETDIR}/OSX/CoreBitcoin.framework
 mv ${BINARIES_TARGETDIR}/OSX/CoreBitcoin.framework/CoreBitcoinOSX ${BINARIES_TARGETDIR}/OSX/CoreBitcoin.framework/CoreBitcoin
 
 mv build/CoreBitcoinIOS.framework ${BINARIES_TARGETDIR}/iOS/CoreBitcoin.framework
 mv ${BINARIES_TARGETDIR}/iOS/CoreBitcoin.framework/CoreBitcoinIOS ${BINARIES_TARGETDIR}/iOS/CoreBitcoin.framework/CoreBitcoin
 
-#move and rename the static libraries
+# Move and rename the static libraries
 mv build/libCoreBitcoinIOS.a ${BINARIES_TARGETDIR}/iOS/libCoreBitcoin.a
 mv build/libCoreBitcoinOSX.a ${BINARIES_TARGETDIR}/OSX/libCoreBitcoin.a
 
-#move the headers
+# Move the headers
 mv build/include ${BINARIES_TARGETDIR}/include
+
+# Clean up
+rm -rf build
+
+# Remove +Tests headers.
+
+
+
