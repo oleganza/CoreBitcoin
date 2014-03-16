3f6c20cb0daa6f5d9a7b0eac7066f57522f0e659
Oleg Andreev <oleganza@gmail.com>
2014-03-16 15:51:01 +0100

    wip on BTCProcessor

---
+++ CoreBitcoin/BTCProcessor.m
@@ -0,0 +1,13 @@
+//
+//  BTCProcessor.m
+//  CoreBitcoin
+//
+//  Created by Oleg Andreev on 15.03.2014.
+//  Copyright (c) 2014 Oleg Andreev. All rights reserved.
+//
+
+#import "BTCProcessor.h"
+
+@implementation BTCProcessor
+
+@end
