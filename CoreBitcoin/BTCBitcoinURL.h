1e62d6d8fd5b1f1b7b754f30ebf99514fc8e86f1
Oleg Andreev <oleganza@gmail.com>
2014-11-05 17:42:46 +0100

    added BTCBitcoinURL to parse and compose Bitcoin URLs (only basic properties are supported for now)

---
+++ CoreBitcoin/BTCBitcoinURL.h
@@ -0,0 +1,51 @@
+#import <Foundation/Foundation.h>
+
+// TODO: support arbitrary keys and values.
+// TODO: support handling URL from UIApplicationDelegate.
+
+/*!
+ * Class to compose and handle various Bitcoin URLs according to BIP21.
+ * See: https://github.com/bitcoin/bips/blob/master/bip-0021.mediawiki
+ */
+@interface BTCBitcoinURL : NSObject
+
+/*!
+ * Encoded address.
+ */
+@property(nonatomic) BTCAddress* address;
+
+/*!
+ * Amount in satoshis. Default is 0.
+ */
+@property(nonatomic) BTCSatoshi amount;
+
+/*!
+ * Label. Default is nil.
+ */
+@property(nonatomic) NSString* label;
+
+/*!
+ * Message. Default is nil.
+ */
+@property(nonatomic) NSString* message;
+
+
+/*!
+ * Makes a URL in form "bitcoin:<address>?amount=1.2345&label=<label>.
+ * @param address Address to be rendered in base58 format.
+ * @param amount  Amount in satoshis. Note that URI scheme dictates to render this amount as a decimal number in BTC.
+ * @param label   Optional label.
+ */
++ (NSURL*) URLWithAddress:(BTCAddress*)address amount:(BTCSatoshi)amount label:(NSString*)label;
+
+/*!
+ * Instantiates if URL is a valid bitcoin: URL.
+ */
+- (id) initWithURL:(NSURL*)url;
+
+/*!
+ * Builds a URL using the properties.
+ */
+- (NSURL*) URL;
+
+@end
---
+++ CoreBitcoin/BTCBitcoinURL.m
@@ -0,0 +1,79 @@
+#import "BTCBitcoinURL.h"
+#import "BTCNumberFormatter.h"
+
+@implementation BTCBitcoinURL
+
++ (NSURL*) URLWithAddress:(BTCAddress*)address amount:(BTCSatoshi)amount label:(NSString*)label
+{
+    if (!address || amount <= 0) return nil;
+
+    NSString* amountString = [self formatAmount:amount];
+
+    NSMutableString* s = [NSMutableString stringWithFormat:@"bitcoin:%@?amount=%@", address.base58String, amountString];
+
+    if (label && label.length > 0)
+    {
+        [s appendFormat:@"&label=%@", [label stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    }
+
+    return [NSURL URLWithString:s];
+}
+
+/*!
+ * Instantiates if URL is a valid bitcoin: URL.
+ */
+- (id) initWithURL:(NSURL*)url
+{
+    if (!url) return nil;
+
+    NSURLComponents* comps = [[NSURLComponents alloc] initWithURL:url resolvingAgainstBaseURL:NO];
+
+    if (!comps) return nil;
+
+    if (![comps.scheme isEqual:@"bitcoin"]) return nil;
+
+    // We allow empty address, but if it's not empty, it must be a valid address.
+    BTCAddress* address = nil;
+    if (comps.path.length > 0)
+    {
+        address = [BTCAddress addressWithBase58String:comps.path];
+        if (!address)
+        {
+            return nil;
+        }
+    }
+
+    if (self = [super init])
+    {
+        self.address = address;
+        for (NSURLQueryItem* item in comps.queryItems)
+        {
+            if ([item.name isEqual:@"amount"])
+            {
+                self.amount = [[self class] parseAmount:item.value];
+            }
+        }
+    }
+    return self;
+}
+
+- (NSURL*) URL
+{
+    return nil;
+}
+
++ (NSString*) formatAmount:(BTCSatoshi)amount
+{
+    return [NSString stringWithFormat:@"%d.%08d", (int)(amount / BTCCoin), (int)(amount % BTCCoin)];
+}
+
++ (BTCSatoshi) parseAmount:(NSString*)string
+{
+    NSLocale* locale = [[NSLocale localeWithLocaleIdentifier:@"en_US"] copy]; // uses period (".") as a decimal point.
+    NSAssert([[locale objectForKey:NSLocaleDecimalSeparator] isEqual:@"."], @"must be point as a decimal separator");
+    NSDecimalNumber* dn = [NSDecimalNumber decimalNumberWithString:string locale:locale];
+    dn = [dn decimalNumberByMultiplyingByPowerOf10:8];
+    return BTCAmountFromDecimalNumber(dn);
+}
+
+@end
