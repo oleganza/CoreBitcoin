6d6c334d74adcda5a14a1f140e8dbc6780d84bf1
Robert Mozayeni <dev@mzy.me>
2015-05-14 10:21:52 -0400

    Added 3 test files

--- CoreBitcoin/SwiftBridgingHeader.h
+++ CoreBitcoin/SwiftBridgingHeader.h
@@ -6,3 +6,11 @@
 //  Copyright (c) 2015 Oleg Andreev. All rights reserved.
 //
 
+#import "BTCBlockchainInfo.h"
+#import "BTCAddress.h"
+#import "BTCTransactionOutput.h"
+#import "BTCPriceSource.h"
+#import "BTCAddress.h"
+#import "NSData+BTCData.h"
+#import "NS+BTCBase58.h"
+#import "BTCKey.h"
