ef5d07a48f9071c02bff78715d62263cb54146a0
Joel Klabo <joelklabo@gmail.com>
2015-12-15 16:22:22 -0800

    Converting bracket style to conform with Apple Objective-C, Swift guidelines.

--- CoreBitcoin/BTCTransactionInput.m
+++ CoreBitcoin/BTCTransactionInput.m
@@ -18,10 +18,8 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
 
 @implementation BTCTransactionInput
 
-- (id) init
-{
-    if (self = [super init])
-    {
+- (id) init {
+    if (self = [super init]) {
         _previousHash = BTCZero256();
         _previousIndex = BTCInvalidIndex;
         _signatureScript = [[BTCScript alloc] init];
@@ -32,63 +30,47 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
 }
 
 // Parses tx input from a data buffer.
-- (id) initWithData:(NSData*)data
-{
-    if (self = [self init])
-    {
+- (id) initWithData:(NSData*)data {
+    if (self = [self init]) {
         if (![self parseData:data]) return nil;
     }
     return self;
 }
 
 // Read tx input from the stream.
-- (id) initWithStream:(NSInputStream*)stream
-{
-    if (self = [self init])
-    {
+- (id) initWithStream:(NSInputStream*)stream {
+    if (self = [self init]) {
         if (![self parseStream:stream]) return nil;
     }
     return self;
 }
 
 // Constructs transaction input from a dictionary representation
-- (id) initWithDictionary:(NSDictionary*)dictionary
-{
-    if (self = [self init])
-    {
+- (id) initWithDictionary:(NSDictionary*)dictionary {
+    if (self = [self init]) {
         NSString* prevHashString = (dictionary[@"prev_out"] ?: @{})[@"hash"];
         if (prevHashString) _previousHash = BTCReversedData(BTCDataFromHex(prevHashString));
         NSNumber* prevIndexNumber = (dictionary[@"prev_out"] ?: @{})[@"n"];
         if (prevIndexNumber) _previousIndex = prevIndexNumber.unsignedIntValue;
         
-        if (dictionary[@"coinbase"])
-        {
+        if (dictionary[@"coinbase"]) {
             _coinbaseData = BTCDataFromHex(dictionary[@"coinbase"]);
-        }
-        else
-        {
+        } else {
             // BitcoinQT RPC creates {"asm": ..., "hex": ...} dictionary for this key instead of an ASM-like string like bitcoin-ruby and bitcoinjs.
             id scriptSig = dictionary[@"scriptSig"];
             
-            if ([scriptSig isKindOfClass:[NSString class]])
-            {
+            if ([scriptSig isKindOfClass:[NSString class]]) {
                 _signatureScript = [[BTCScript alloc] initWithString:scriptSig];
-            }
-            else if ([scriptSig isKindOfClass:[NSDictionary class]])
-            {
-                if (scriptSig[@"hex"])
-                {
+            } else if ([scriptSig isKindOfClass:[NSDictionary class]]) {
+                if (scriptSig[@"hex"]) {
                     _signatureScript = [[BTCScript alloc] initWithData:BTCDataFromHex(scriptSig[@"hex"])];
-                }
-                else if (scriptSig[@"asm"])
-                {
+                } else if (scriptSig[@"asm"]) {
                     _signatureScript = [[BTCScript alloc] initWithString:scriptSig[@"asm"]];
                 }
             }
         }
         
-        if (!_signatureScript)
-        {
+        if (!_signatureScript) {
             return nil;
         }
         
@@ -99,8 +81,7 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
     return self;
 }
 
-- (id) copyWithZone:(NSZone *)zone
-{
+- (id) copyWithZone:(NSZone *)zone {
     BTCTransactionInput* txin = [[BTCTransactionInput alloc] init];
     txin.previousHash = [self.previousHash copy];
     txin.previousIndex = self.previousIndex;
@@ -114,25 +95,20 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
     return txin;
 }
 
-- (NSData*) data
-{
+- (NSData*) data {
     return [self computePayload];
 }
 
-- (NSData*) computePayload
-{
+- (NSData*) computePayload {
     NSMutableData* payload = [NSMutableData data];
     
     [payload appendData:_previousHash];
     [payload appendBytes:&_previousIndex length:4];
 
-    if (self.isCoinbase)
-    {
+    if (self.isCoinbase) {
         [payload appendData:[BTCProtocolSerialization dataForVarInt:self.coinbaseData.length]];
         [payload appendData:self.coinbaseData];
-    }
-    else
-    {
+    } else {
         NSData* scriptData = _signatureScript.data;
         [payload appendData:[BTCProtocolSerialization dataForVarInt:scriptData.length]];
         [payload appendData:scriptData];
@@ -143,55 +119,45 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
     return payload;
 }
 
-- (BTCOutpoint*) outpoint
-{
+- (BTCOutpoint*) outpoint {
     return [[BTCOutpoint alloc] initWithHash:self.previousHash index:self.previousIndex];
 }
 
-- (void) setOutpoint:(BTCOutpoint *)outpoint
-{
+- (void) setOutpoint:(BTCOutpoint *)outpoint {
     self.previousHash = outpoint.txHash;
     self.previousIndex = outpoint.index;
 }
 
-- (NSString*) previousTransactionID
-{
+- (NSString*) previousTransactionID {
     return BTCIDFromHash(self.previousHash);
 }
 
-- (void) setPreviousTransactionID:(NSString *)previousTransactionID
-{
+- (void) setPreviousTransactionID:(NSString *)previousTransactionID {
     self.previousHash = BTCHashFromID(previousTransactionID);
 }
 
 // Returns a dictionary representation suitable for encoding in JSON or Plist.
-- (NSDictionary*) dictionaryRepresentation
-{
+- (NSDictionary*) dictionaryRepresentation {
     return self.dictionary;
 }
 
-- (NSDictionary*) dictionary
-{
+- (NSDictionary*) dictionary {
     NSMutableDictionary* dict = [NSMutableDictionary dictionary];
     dict[@"prev_out"] = @{
                         @"hash": BTCHexFromData(BTCReversedData(_previousHash)), // transaction hashes are reversed
                         @"n": @(_previousIndex),
                         };
     
-    if ([self isCoinbase])
-    {
+    if ([self isCoinbase]) {
         dict[@"coinbase"] = BTCHexFromData(_coinbaseData);
-    }
-    else
-    {
+    } else {
         // Latest BitcoinQT does this: "scriptSig": {"asm": "assembly-style script with spaces and literal ops", "hex": "raw script in hex" }
         // Here and in bitcoin-qt we use only "asm" style script.
         // (But we support reading BitcoinQT style in initWithDictionary:)
         dict[@"scriptSig"] = _signatureScript.string;
     }
     
-    if (_sequence != BTCMaxSequence)
-    {
+    if (_sequence != BTCMaxSequence) {
         dict[@"sequence"] = [NSString stringWithFormat:@"%08x", _sequence];
     }
     return dict;
@@ -208,8 +174,7 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
 
 
 
-- (BOOL) parseData:(NSData*)data
-{
+- (BOOL) parseData:(NSData*)data {
     if (!data) return NO;
     NSInputStream* stream = [NSInputStream inputStreamWithData:data];
     [stream open];
@@ -218,8 +183,7 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
     return result;
 }
 
-- (BOOL) parseStream:(NSInputStream*)stream
-{
+- (BOOL) parseStream:(NSInputStream*)stream {
     if (!stream) return NO;
     if (stream.streamStatus == NSStreamStatusClosed) return NO;
     if (stream.streamStatus == NSStreamStatusNotOpen) return NO;
@@ -236,12 +200,9 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
     NSData* scriptdata = [BTCProtocolSerialization readVarStringFromStream:stream];
     if (!scriptdata) return NO;
 
-    if ([self isCoinbase])
-    {
+    if ([self isCoinbase]) {
         _coinbaseData = scriptdata;
-    }
-    else
-    {
+    } else {
         _signatureScript = [[BTCScript alloc] initWithData:scriptdata];
     }
     
@@ -252,8 +213,7 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
 }
 
 
-- (BOOL) isCoinbase
-{
+- (BOOL) isCoinbase {
     return (_previousIndex == BTCInvalidIndex) &&
             _previousHash.length == 32 &&
             0 == memcmp(BTCZeroString256(), _previousHash.bytes, 32);
@@ -265,8 +225,7 @@ static const uint32_t BTCMaxSequence = 0xFFFFFFFF;
 
 
 
-- (BTCAmount) value
-{
+- (BTCAmount) value {
     if (_value != -1) {
         return _value;
     }
